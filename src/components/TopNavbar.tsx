540aa5fce66f34bdf85829cc87fd02e9c6650c24
unknown <bl4ckfl4me@yandex.ru>
2023-12-27 11:14:15 +0400

    update topnav logo and title

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -7,16 +7,13 @@ import {
 import { Bars2Icon } from "@heroicons/react/24/outline";
 import ProfileMenu from "./ProfileMenu";
 import NavList from "./NavList";
-import React, { useEffect } from "react";
+import React from "react";
 import AuthPanel from "./AuthPanel";
 import { HOME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
-import { useAppDispatch, useAppSelector } from "../hooks";
-import { updateAccessToken } from "../store/userSlice";
+import { useAppSelector } from "../hooks";
 
 const TopNavbar = () => {
-  const { user, accessToken, refreshToken } = useAppSelector(
-    (state) => state.user
-  );
+  const { user } = useAppSelector((state) => state.user);
 
   const [isNavOpen, setIsNavOpen] = React.useState(false);
   const isLoginRoute =
@@ -24,7 +21,7 @@ const TopNavbar = () => {
     location.pathname === REGISTRATION_ROUTE;
 
   return (
-    <Navbar className="sticky max-w-full overflow-hidden top-0 z-50 h-max p-2 lg:rounded-full lg:px-6 lg:py-4">
+    <Navbar className="sticky max-w-full overflow-hidden top-0 z-50 h-max p-2 lg:rounded-md lg:px-6 lg:py-4">
       <div className="relative mx-auto flex justify-between items-center text-blue-gray-900">
         <IconButton
           size="sm"
@@ -35,13 +32,20 @@ const TopNavbar = () => {
         >
           <Bars2Icon className="h-6 w-6" />
         </IconButton>
-        <Typography
-          as="a"
-          href={HOME_ROUTE}
-          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
-        >
-          Online School
-        </Typography>
+        <div className="flex justify-between gap-x-3">
+          <img
+            className="h-10 w-10 rounded-sm object-cover object-center"
+            src="..\\..\\static\\img\\lfNQEIDnNPA.jpg"
+            alt="nature image"
+          />
+          <Typography
+            as="a"
+            href={HOME_ROUTE}
+            className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
+          >
+            Онлайн школа
+          </Typography>
+        </div>
         <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
           <NavList />
         </div>
