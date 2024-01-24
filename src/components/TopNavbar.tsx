ac0c9a816a278f3a20f6c5565b902f5c6fa93e09
Desen123 <gder-korepanov@yandex.ru>
2024-01-24 20:28:40 +0400

    все

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -7,18 +7,20 @@ import {
 import { Bars2Icon } from "@heroicons/react/24/outline";
 import ProfileMenu from "./ProfileMenu";
 import NavList from "./NavList";
-import React from "react";
 import AuthPanel from "./AuthPanel";
 import { HOME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 import { useAppSelector } from "../hooks";
+import { useState } from "react";
 
 const TopNavbar = () => {
   const { user } = useAppSelector((state) => state.user);
 
-  const [isNavOpen, setIsNavOpen] = React.useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const isLoginRoute =
     location.pathname === LOGIN_ROUTE ||
     location.pathname === REGISTRATION_ROUTE;
+
+  console.log(user.isLoggedIn);
 
   return (
     <Navbar className="sticky max-w-full overflow-hidden top-0 z-50 h-max p-2 lg:rounded-md lg:px-6 lg:py-4">
@@ -46,14 +48,18 @@ const TopNavbar = () => {
             Онлайн школа
           </Typography>
         </div>
-        <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
-          <NavList />
-        </div>
+        {user.isLoggedIn && (
+          <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
+            <NavList />
+          </div>
+        )}
         {user.isLoggedIn ? <ProfileMenu /> : !isLoginRoute && <AuthPanel />}
       </div>
-      <Collapse open={isNavOpen}>
-        <NavList />
-      </Collapse>
+      {user.isLoggedIn && (
+        <Collapse open={isNavOpen}>
+          <NavList />
+        </Collapse>
+      )}
     </Navbar>
   );
 };
